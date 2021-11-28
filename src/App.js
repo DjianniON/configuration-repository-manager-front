2ee9ceadeb1ea096e900fb113aca030d5771cae6
Djianni <Djianni.orrunowicki@gmail.com>
2021-11-28 15:59:29 +0100

    added routing packages + create page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Read from './components/read';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       Projet DA50 - Front
+      <div>
+          <Route exact path='/read' component={Read} />
+        </div>
     </div>
   );
 }
