a48d10c4a2c6d5e558236c6e33f523e2e008fe4a
louischdt <misterchardon@gmail.com>
2021-12-27 19:25:38 +0100

    ca marche mieux

---
+++ src/components/form/paraForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Form, Button, TextArea } from 'semantic-ui-react';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function ParaForm(props) {
+    let navigation = useNavigate();
+    let onSubmit = (e) => {
+        e.preventDefault();
+        props.operation();
+    }
+
+
+    return (
+        <Form onSubmit={onSubmit}>
+            <Form.Field required>
+                <label>Nom</label>
+                <input placeholder='Nom' value={props.nom} onChange={(e) => props.setNom(e.target.value)} required />
+            </Form.Field>
+            <Form.Field>
+                <label>Type</label>
+                <TextArea placeholder='Type' value={props.type} onChange={(e) => props.setType(e.target.value)} />
+            </Form.Field>
+            <Form.Field>
+                <label>Valeur</label>
+                <TextArea placeholder='Valeur' value={props.valeur} onChange={(e) => props.setValeur(e.target.value)} />
+            </Form.Field>
+            <Button type='submit' color='green'>{props.type === "update" ? "Mettre à jour" : "Créer"}</Button>
+            <Button type='button' onClick={() => navigation(-1)} color='red'>Annuler</Button>
+        </Form>
+    )
+}
+
